00a6b7a85c47bc01430535fb3879b24e3b138a12
abdulahad0110 <abdulahad200325@gmail.com>
2024-10-24 02:29:11 +0500

    first commit

--- app/page.js
+++ app/page.js
@@ -6,45 +6,45 @@ import { useState } from 'react';
 
 export default function Home() {
 
-  const [quote, setQuote] = useState('');
+    const [quote, setQuote] = useState('');
 
-  useEffect(() => {
-    getRandomQuote()
-  }, [])
+    useEffect(() => {
+        getRandomQuote()
+    }, [])
 
-  const getRandomQuote = async () => {
-    const URL = "https://dummyjson.com/quotes"
-    const response = await axios.get(URL);
-    const randomIndex = Math.floor(Math.random() * 30)
-    const randomQuote = response.data.quotes[randomIndex];
-    setQuote(randomQuote);
-    console.log(randomQuote);
-  }
+    const getRandomQuote = async () => {
+        const URL = "https://dummyjson.com/quotes"
+        const response = await axios.get(URL);
+        const randomIndex = Math.floor(Math.random() * 30)
+        const randomQuote = response.data.quotes[randomIndex];
+        setQuote(randomQuote);
+        console.log(randomQuote);
+    }
 
 
-  return (
-    <div className="min-h-screen p-6 flex items-center justify-center bg-gradient-to-r from-[#50C9C3] to-[#96DEDA]">
-      <div className="md:w-1/2 p-6 bg-white shadow-xl rounded-lg">
-        <h1 className="text-5xl font-bold mb-4 text-black text-center">
-          Random Quote
-        </h1>
+    return (
+        <div className="min-h-screen p-6 flex items-center justify-center bg-gradient-to-r from-[#50C9C3] to-[#96DEDA]">
+            <div className="md:w-1/2 p-6 bg-white shadow-xl rounded-lg">
+                <h1 className="text-5xl font-bold mb-4 text-black text-center">
+                    Random Quote
+                </h1>
 
-        <div className="bg-[#0eaad5] p-6 rounded-md">
-          <p className="text-white italic text-3xl">
-            "{quote?.quote || 'Loading...'}"
-          </p>
-          <p className="mt-4 text-right font-medium text-lg text-white">
-            — {quote?.author || ''}
-          </p>
+                <div className="bg-[#0eaad5] p-6 rounded-md">
+                    <p className="text-white italic text-3xl">
+                        "{quote?.quote || 'Loading...'}"
+                    </p>
+                    <p className="mt-4 text-right font-medium text-lg text-white">
+                        — {quote?.author || ''}
+                    </p>
+                </div>
+
+                <button
+                    onClick={getRandomQuote}
+                    className="mt-6 text-xl bg-[#0eaad5] text-white py-2 px-4 rounded-lg active:scale-90 transition-colors w-full"
+                >
+                    New Quote
+                </button>
+            </div>
         </div>
-
-        <button
-          onClick={getRandomQuote}
-          className="mt-6 text-xl bg-[#0eaad5] text-white py-2 px-4 rounded-lg active:scale-90 transition-colors w-full"
-        >
-          New Quote
-        </button>
-      </div>
-    </div>
-  );
+    );
 }
